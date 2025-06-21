7737623c662944b9bf2d8b8edccaa1456e825630
Nick Gore <goreglyad@gmail.com>
2025-06-21 19:55:05 +0200

    sequrity groups, NAT GW, bastion, adjust gh action workflow accordingly

---
+++ network/security_groups.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from your IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.your_ip_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  name        = "private-sg"
+  description = "Allow internal and outbound"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow all from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- network/variables.tf
+++ network/variables.tf
@@ -19,3 +19,18 @@ variable "private_subnet_cidrs" {
 variable "availability_zones" {
   default = ["eu-north-1a", "eu-north-1b"]
 }
+
+variable "your_ip_cidr" {
+  description = "Your IP address in CIDR format (e.g. 1.2.3.4/32)"
+  type        = string
+}
+
+variable "bastion_ami_id" {
+  description = "AMI ID for bastion host"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key name"
+  type        = string
+}
