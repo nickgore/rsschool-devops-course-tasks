e7781acd962727cb74d70853fe40b200cb7f4c5c
Nick Gore <goreglyad@gmail.com>
2025-06-25 16:04:16 +0200

    change vm type for bastion host

--- network/bastion.tf
+++ network/bastion.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "bastion" {
   ami                         = var.bastion_ami_id # Amazon Linux 2 or Ubuntu
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public[0].id
   associate_public_ip_address = true
   security_groups             = [aws_security_group.bastion_sg.id]
